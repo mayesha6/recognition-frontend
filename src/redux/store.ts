db24138dacc52ddad040cda5a86cd51bf5817e86
mayesha6 <soumyrhmn234@gmail.com>
2026-07-15 09:39:41 +0600

    redux- store

---
+++ src/redux/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { rootReducer } from "./features/rootReducer";
+import { baseApi } from "./api/baseApi";
+
+const persistConfig = { key: "root", storage, whitelist: ["auth"] };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (gDM) => gDM({ serializableCheck: false }).concat(baseApi.middleware),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
