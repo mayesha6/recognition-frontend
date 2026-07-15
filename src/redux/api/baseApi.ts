795c4006196fb255513be2bdc29770860c2ce7ab
mayesha6 <soumyrhmn234@gmail.com>
2026-07-15 09:38:54 +0600

    redux- base api

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) headers.set("Authorization", `Bearer ${token}`);
+      return headers;
+    },
+  }),
+  tagTypes: ["User"],
+  endpoints: () => ({}),
+});
